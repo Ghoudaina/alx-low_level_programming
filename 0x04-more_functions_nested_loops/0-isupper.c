de2db91653a86e191404a0171c503183575e75f2
Ghoudaina <elomaryroudaina@gmail.com>
2023-02-23 08:05:44 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+
+/**
+ * i-supper -check if a letter is uppercase
+ * @x: the number to be checked 
+ * Return: 1 for upper case letter or 0 for anything else
+ */
+
+int -isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+		return(1);
+	}
+		return(0);
+}
